3a3d07241b5272a77585c09efcbb0bcef42840cb
Justin Palmer <encytemedia@gmail.com>
2021-12-15 17:32:51 -0800

    fix knob value

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
+  import "input-knob";
   import Knob from "./Knob.svelte";
 </script>
 
 <div>
-  hey
-
-  <Knob value={12} label="foo" />
+  <div class="grid grid-cols-2 gap-2 p-5 w-1/3">
+    <div class="w-12 h-12">
+      <Knob value={12} label="Pitch" />
+    </div>
+    <div class="w-12 h-12">
+      <Knob value={12} label="Cent" />
+    </div>
+  </div>
 </div>
 
 <style>
--- src/Knob.svelte
+++ src/Knob.svelte
@@ -1,15 +1,69 @@
 <script type="ts">
   export let label: string = "";
   export let value: number;
+  export let format: (value: number) => number = (value) => Math.round(value);
+
+  let moving: boolean = false;
+
+  const onKnobMoveStart = (e: Event) => {
+    moving = true;
+    return true;
+  };
+
+  const onKnobMoveEnd = (e: Event) => {
+    moving = false;
+    return true;
+  };
+
+  const onKnobMoveChange = (e: Event) => {
+    const target = e.target as HTMLInputElement;
+    if (target) value = format(target.value);
+  };
 </script>
 
 <div class="inline-block">
-  {label} label
-  <input-knob value="0" scale="100" min="0" max="100">
-    <div class="mark">&bull;</div>
-    {value}
+  <div class="text-center mb-1 text-xs">
+    <span class:moving>{label}</span>
+    <span class:moving={!moving}>{value}</span>
+  </div>
+  <input-knob
+    scale="100"
+    min="0"
+    max="100"
+    on:knob-move-start={onKnobMoveStart}
+    on:knob-move-end={onKnobMoveEnd}
+    on:knob-move-change={onKnobMoveChange}
+  >
+    <svg viewBox="0 0 100 100" class="w-full h-full text-white text-opacity-50">
+      <circle
+        cx="50%"
+        cy="50%"
+        r="40"
+        stroke="currentColor"
+        stroke-width="9"
+        stroke-dasharray="3 6"
+        fill="none"
+      />
+      <circle
+        class="stroke-black"
+        cx="50%"
+        cy="50%"
+        r="44"
+        stroke="currentColor"
+        stroke-width="6"
+        fill="none"
+      />
+      <rect x="48" y="6" width="4" height="20" fill="white" />
+    </svg>
   </input-knob>
 </div>
 
 <style>
+  input-knob::part(rotator) {
+    @apply relative box-border border-double border rounded-full bg-gray-700 border-black;
+  }
+
+  .moving {
+    @apply hidden;
+  }
 </style>
